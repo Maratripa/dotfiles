d40c81422ac72415aa2cc1ac4bc8e046ef58fda9
Maratripa <maratripa@gmail.com>
2022-12-26 23:22:06 -0300

    Wezterm configuration

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+return {
+    -- Appearance
+    font = wezterm.font "JetBrains Mono",
+
+    color_scheme = "Catppuccin Mocha",
+
+    -- Scrollback
+    scrollback_lines = 3500,
+}
