1725e104674057d3c3c5134292c1d1aba06dffd2
Maratripa <maratripa@gmail.com>
2022-12-27 01:03:28 -0300

    Fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Alias
+
+# ls -> exa
+if type -q exa
+    alias l 'exa -1 --icons'
+    alias la 'exa -l -a -h --icons --classify --group-directories-first --no-user'
+    alias ld 'exa -l -a -h --list-dirs --no-user'
+    alias ll 'exa -l -a -h --git --no-user'
+    alias lt='exa -a --no-user --tree --level'
+end
+
+# Abbr
+
+# git 
+abbr -a -g gst 'git status -sb'
+
+# mkdir
+abbr -a -g mkdir "mkdir -pv"
+
+# ls
+abbr -a -g ls "exa -1 --icons"
+
+
