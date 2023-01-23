4f20045100e4f91838ffa566a365488f664cdcb3
Maratripa <maratripa@gmail.com>
2023-01-23 01:38:13 -0300

    disable tab bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,21 @@
 local wezterm = require("wezterm")
 
+local function window_padding(margins)
+	return {
+		top = margins,
+		bottom = margins,
+		right = margins,
+		left = margins
+	}
+end
+
 return {
     -- Appearance
     font = wezterm.font "JetBrains Mono",
-
+    line_height = 1.1,
+    enable_tab_bar = false,
     color_scheme = "Catppuccin Mocha",
+    window_padding = window_padding(20),
 
     -- Scrollback
     scrollback_lines = 3500,
